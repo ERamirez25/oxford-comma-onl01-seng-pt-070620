3c3ec104fb278c1b4a81ded1cbe15ad8468c379b
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-13 23:40:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
 elsif array.length == 2 
   array.join(" and ")
 else 
-  last_word = array[-1].insert(0,"and")
+  last_word = array[-1].insert(0,"and ")
   #array[0..-2].join(", ")
 array
 end 
